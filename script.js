05071b8a69a7436a8276edc30863cd95a2f6cf83
Gagan <gagansodhi751@gmail.com>
2023-11-03 15:19:08 -0600

    Project13/3

---
+++ script.js
@@ -0,0 +1,14 @@
+//  Create a function that takes an integer as an argument and returns "Even" for even numbers
+//  or "Odd" for odd numbers. This code is from codewars.com
+function  fInt(int1) {
+    if(int1%2 === 0)
+    {
+        console.log("This is even number");
+    }
+    else
+    {
+        console.log("This is odd number");
+    }
+}
+
+fInt(8);
